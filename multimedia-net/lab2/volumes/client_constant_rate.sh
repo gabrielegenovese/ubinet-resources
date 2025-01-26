d62bb51a1b2a8c28282a8f55e903d4da3aab6ff8
geno <gabriele.genovese2@studio.unibo.it>
2025-01-26 22:39:30 +0100

    update

--- multimedia-net/lab2/volumes/client_constant_rate.sh
+++ multimedia-net/lab2/volumes/client_constant_rate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "fixing bw to 1 Mbps from the beginning"
 tc qdisc del dev eth0 root
-tc qdisc add dev eth0 root netem rate 1mbit delay 10ms
+tc qdisc add dev eth0 root netem rate 10mbit delay 10ms
